f5acf405503f4cd2ebb02e70d8b65ebff8679c71
Aswanth UP <aswanthup06@gmail.com>
2026-08-12 13:26:41 +0530

    build error fix

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import Functionalities from "./components/Functionalities";
 import WhyChoose from "./components/WhyChoose";
 import Newsletter from "./components/Newsletter";
 
-export default function BeinexLanding() {
+export default function Home() {
   return (
     <div className="min-h-screen bg-white text-slate-900 font-sans">
       <Hero />
